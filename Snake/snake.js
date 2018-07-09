32ec73dd667fc51b3184d23a3380905ca81f8a11
Sergio <sergio.sanchis@est.fib.upc.edu>
2018-07-09 12:16:02 +0200

    Fruit added

--- Snake/snake.js
+++ Snake/snake.js
@@ -4,9 +4,21 @@ var FRUIT_SIZE = 10;
 var SNAKE_SIZE = 10;
 var WIDTH = 300;
 var HEIGHT = 300;
+var ctx;
 
-function init() {
-
+function initialize() {
+	ctx = document.getElementById("snake-screen").getContext("2d");
+	updateFruit();
 }
 
-window.onload = init;
+function updateFruit() {
+	//random of 1 to max_canvas/snake_size + 1) x snake_size
+	var fruitPositionX = (Math.floor(Math.random()*(WIDTH/SNAKE_SIZE)) + 1 ) * SNAKE_SIZE;
+	var fruitPositionY = (Math.floor(Math.random()*(HEIGHT/SNAKE_SIZE)) + 1 ) * SNAKE_SIZE;
+	ctx.beginPath();
+	ctx.arc(fruitPositionX,fruitPositionY,FRUIT_SIZE,0, Math.PI*2, true);
+	ctx.fillStyle = "red";
+	ctx.fill();
+}
+
+window.onload = initialize;
