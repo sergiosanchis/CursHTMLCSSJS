957c5576ee866c39da29ba9b89e48e9e2e21f93b
Sergio <sergio.sanchis@est.fib.upc.edu>
2018-07-21 20:29:20 +0200

    Project first version done

---
+++ project/form/form.js
@@ -0,0 +1,7 @@
+
+function showData(){
+    var name = document.forms["myForm"]["name"].value;
+    var password = document.forms["myForm"]["password"].value;
+    window.alert("Les dades enviades son: \n"+
+    "Name: "+ name + "\nPassword: " + password)
+}
